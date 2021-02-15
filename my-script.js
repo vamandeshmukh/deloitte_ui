bf1d540ded9924c52a5ce54894572bdd2431e5bd
vamandeshmukh <vamandeshmukh@gmail.com>
2021-02-15 13:26:29 +0530

    Commit
    Commit

---
+++ my-script.js
@@ -0,0 +1,20 @@
+
+console.log("Hello world!");
+
+
+function fun() {
+    console.log("fun");
+}
+
+fun();
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(10));
+console.log(add(10, 20));
+console.log(add(10, 20, 30));
+console.log(add("Java", "Script")); 
+
+
