b43e5193cde89adb2acb2dbaba08deda7426cf23
vamandeshmukh <vamandeshmukh@gmail.com>
2021-02-16 10:46:47 +0530

    Commit
    Commit

---
+++ 01-javascript-basics.js
@@ -0,0 +1,121 @@
+
+// java style 
+// int num = 10; 
+// String name = "Sonu";
+
+// JS style 
+// number, string, boolean 
+
+// num = 10; 
+// var num2 = 20; 
+
+// let num3 = 30;
+// const num4 = 40; // final in Java 
+
+// let runs = 0; 
+// const numberOfballs = 120; 
+
+// truthy falsy 
+// falsy - 0, "", null, NaN, undefined 
+// truthy - rest all 
+
+// let names;
+// names = "Sonu";
+
+// if (names) {
+//     console.log("Hi" + names);
+// }
+// else {
+//     console.log("No");
+// }
+
+// NaN 
+// function fun(a, b) {
+//     return a + b;
+// }
+
+// console.log(fun(10));
+
+// let num = true;
+
+// console.log(num);
+
+// String  
+
+
+// let title = `Ms`
+// let firstName = 'Sonu';
+// // let fullName = firstName + ' Joshi';
+
+// let fullName = `${title} ${firstName} Joshi`;
+
+// console.log(fullName);
+
+
+// function fun(a, b) {
+//     return a + b;
+// }
+
+// variable hoisting 
+
+// console.log(num); // undefined 
+// var num = 10; 
+// console.log(num); // 10 
+
+// console.log(fun(10, 20));
+
+// function fun(a, b) {
+//     return a + b;
+// }
+
+
+// let a = 10;
+// let b = "10";
+// console.log(a == b); // 
+// console.log(a === b); // typesafe conparison 
+
+// function fun2() {
+//     console.log(`fun2`);
+// }
+
+// const fun = (a, b) => { return a + b};
+
+
+// console.log(this);
+
+'use strict' // makes JS typesafe  
+
+// let num = 10;
+// console.log(num);
+// console.log(typeof(num));
+
+// num = "Abc";
+// console.log(num);
+// console.log(typeof(num));
+
+// num = true;
+// console.log(num);
+// console.log(typeof(num));
+
+// objects in JS 
+
+// let emp = {
+//     'name': 'Sonu',
+//     'age': 10,
+//     phones: {
+//         'home': 1010,
+//         'office': 2020
+//     }
+// };
+
+// console.log(emp);
+
+// class MyClass {
+
+// }
+
+// let myClass = new MyClass();
+
+let myNums = [10, 20, 30, 40, 50];
+console.log(myNums);
+
